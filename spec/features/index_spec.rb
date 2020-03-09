89be0cd53ee9dc59fbaac6d22e3b50f32999fbff
Mauricio Scioville <invasormou@gmail.com>
2020-03-09 09:21:49 -0500

    update test

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -17,10 +17,12 @@ RSpec.feature "User see the index page" do
   end 
   
   scenario "clicking a post should redirect to the show page" do
-    lastest_post = posts(:rspec)
+    post = posts(:rspec)
+    post2 = posts(:capybara)
     
     visit root_path
-    expect(page).to have_text lastest_post.title
+    expect(page).to have_text post.title
+    expect(page).to_not have_text post2.title
   end 
   
 end
